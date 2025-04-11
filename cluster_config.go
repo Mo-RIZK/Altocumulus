1b2ff44391c9b21d09c5f4b1dffbd94522c85b18
Mohammad Rizk <mrizk@ptb-03230231.irisa.fr>
2025-04-11 14:22:48 +0200

    Altocumulus v1

--- cluster_config.go
+++ cluster_config.go
@@ -38,7 +38,7 @@ const (
 	DefaultLeaveOnShutdown                 = false
 	DefaultPinOnlyOnTrustedPeers           = false
 	DefaultPinOnlyOnUntrustedPeers         = false
-	DefaultDisableRepinning                = true
+	DefaultDisableRepinning                = false
 	DefaultPeerstoreFile                   = "peerstore"
 	DefaultConnMgrHighWater                = 400
 	DefaultConnMgrLowWater                 = 100
