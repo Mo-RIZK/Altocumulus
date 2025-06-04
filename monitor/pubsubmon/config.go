6c733b60337612299a23f9b72d51186cda399fcd
Mo-RIZK <137156501+Mo-RIZK@users.noreply.github.com>
2025-06-05 00:10:14 +0200

    Update config.go

--- monitor/pubsubmon/config.go
+++ monitor/pubsubmon/config.go
@@ -14,7 +14,7 @@ const envConfigKey = "cluster_pubsubmon"
 
 // Default values for this Config.
 const (
-	DefaultCheckInterval = 15 * time.Second
+	DefaultCheckInterval = 3 * time.Second
 )
 
 // Config allows to initialize a Monitor and customize some parameters.
