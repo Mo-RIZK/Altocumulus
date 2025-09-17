8ff4e8c8008bee634ed8b8e8703ce6fa5d4dae9f
Mo-RIZK <137156501+Mo-RIZK@users.noreply.github.com>
2025-09-17 20:27:27 +0200

    Update config.go

--- informer/disk/config.go
+++ informer/disk/config.go
@@ -14,7 +14,7 @@ const envConfigKey = "cluster_disk"
 
 // Default values for disk Config
 const (
-	DefaultMetricTTL  = 30 * time.Second
+	DefaultMetricTTL  = 2 * time.Second
 	DefaultMetricType = MetricFreeSpace
 )
 
