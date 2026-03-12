04c4aaa1fa8320ddb1d52588d9ee1e2abb51b0cd
Mohammad Rizk <mrizk@ptb-03230231.irisa.fr>
2026-03-12 15:34:23 +0100

    add

--- ECRepairCurrently.go
+++ ECRepairCurrently.go
@@ -140,7 +140,7 @@ func (spt *ECRepairS) pin(op *api.Pin) error {
 	//              TODO: USE this in the new repair with respect to similarities.
 	//
 	//CIDs := op.Metadata["Cids"]
-	download, repair, waittosend := spt.repinUsingRSWithSwitchingNew(op)
+	download, repair, waittosend := spt.repinUsingRSWithSwitching(op)
 	//download, repair, waittosend := spt.repinUsingRSrelatedWork(op)
 	fmt.Fprintf(os.Stdout, "Time Taken to download chunks is : %s and to repair chunks is : %s and additional time to wait to complete sending the shard : %s \n", download.String(), repair.String(), waittosend.String())
 	fmt.Fprintf(os.Stdout, "Date end inside the pintracker repair %s : %s \n", op.Name, time.Now().Format("2006-01-02 15:04:05.000"))
