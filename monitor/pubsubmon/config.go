e87296ba0358a4a6038f3624dfa2255580f098a7
Mohammad Rizk <mrizk@ptb-03230231.irisa.fr>
2026-04-26 14:10:57 +0200

    repair

--- monitor/pubsubmon/config.go
+++ monitor/pubsubmon/config.go
@@ -14,7 +14,7 @@ const envConfigKey = "cluster_pubsubmon"
 
 // Default values for this Config.
 const (
-	DefaultCheckInterval = 40 * time.Second
+	DefaultCheckInterval = 25 * time.Second
 )
 
 // Config allows to initialize a Monitor and customize some parameters.
